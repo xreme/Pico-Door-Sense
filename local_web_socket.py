a8d0686a1329a07bec925c9571727fa6c2497a5d
osereme <reme.ibazebo@gmail.com>
2023-06-11 09:08:51 -0400

    "web_server "->  "local_web_socket"

--- local_web_socket.py
+++ local_web_socket.py
@@ -2,82 +2,58 @@ import network
 import socket
 from time import sleep
 from picozero import pico_led
-import machine
-import server
 
-#Connect to the Wi-Fi
-def connect():
-    #open the file containing the ssid and password
-    config_info_file = open('config.txt', 'r')
+class LocalWebSocket:
 
-    #read the ssid and password
-    config_info = config_info_file.readlines()
-    ssid = config_info[0].strip()
-    password = config_info[1].strip()
+    def __init__(self):
+        pico_led.on()
+        #connect to the network
+        self.ip = self.connect()
+        pico_led.on()
+        #open a socket
+        self.connection = self.open_socket(self.ip)
+        pico_led.off()
 
-    #close the file
-    config_info_file.close()
-    
-    #create an instance of the WLAN class
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    wlan.connect(ssid, password)
-    
-    #continue to send requests to connect to the network
-    #until the connection is successful
-    #FUTUTRE: add a timeout
-    count = 0
-    while not wlan.isconnected():
-        turn_led_off()
-        print('Waiting for Connection ' + str(count))
-        count +=1
-        turn_led_on()
-        sleep(1)
-    
-    turn_led_off()
-    #get the pico's local IP address
-    ip = wlan.ifconfig()[0]
-    print(f'Connected on {ip}')
-    return ip
+    #Connect to the Wi-Fi
+    def connect(self):
+        #open the file containing the ssid and password
+        config_info_file = open('config.txt', 'r')
 
-#Opening a socket for web traffic
-def open_socket(ip):
-    address = (ip, 80) #create a tuple
-    connection = socket.socket() #create a socket and storing it in connection
-    connection.bind(address)  #linking the created socket with the address info
-    connection.listen(1)  #start listening for connections (1 at a time)
-    print(connection)
-    return connection
+        #read the ssid and password
+        config_info = config_info_file.readlines()
+        ssid = config_info[0].strip()
+        password = config_info[1].strip()
 
-#Serve the client requests
-def serve(connection):
-    turn_led_on()
-    # make a new server RequestHandler instance
-    server_instance = server.RequestHandler(connection)
-    turn_led_off()
+        #close the file
+        config_info_file.close()
+        
+        #create an instance of the WLAN class
+        wlan = network.WLAN(network.STA_IF)
+        wlan.active(True)
+        wlan.connect(ssid, password)
+        
+        #continue to send requests to connect to the network
+        #until the connection is successful
+        #FUTUTRE: add a timeout
+        count = 0
+        while not wlan.isconnected():
+            pico_led.off()
+            print('Waiting for Connection ' + str(count))
+            count +=1
+            pico_led.on()
+            sleep(1)
+        
+        pico_led.off()
+        #get the pico's local IP address
+        ip = wlan.ifconfig()[0]
+        print(f'Connected on {ip}')
+        return ip
 
-    #start serving the client requests
-    while True:
-        server_instance.serve()
-
-def turn_led_on():
-    pico_led.on()
-    
-def turn_led_off():
-    pico_led.off()
-
-try:
-    turn_led_on()
-
-    #connect to the network
-    ip = connect()
-
-    turn_led_on()
-    #open a socket
-    connection = open_socket(ip)
-    turn_led_off()
-
-    #start serving the client requests
-    serve(connection)
-except KeyboardInterrupt:
-    machine.reset()
+    #Opening a socket for web traffic
+    def open_socket(self,ip):
+        address = (ip, 80) #create a tuple
+        connection = socket.socket() #create a socket and storing it in connection
+        connection.bind(address)  #linking the created socket with the address info
+        connection.listen(1)  #start listening for connections (1 at a time)
+        print(connection)
+        return connection
---
+++ main.py
@@ -0,0 +1,24 @@
+import local_web_socket
+from picozero import pico_led
+import server
+import machine
+
+
+
+try:
+    #make a new local socket instance
+    local_socket = local_web_socket.LocalWebSocket()
+
+    pico_led.on()
+
+    # make a new server RequestHandler instance
+    server_instance = server.RequestHandler(local_socket.connection)
+
+    pico_led.off()
+
+    #start serving the client requests
+    while True:
+        server_instance.serve()
+
+except KeyboardInterrupt:
+    machine.reset()
